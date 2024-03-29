6d7e3434fe1973aa7d7f915c35d7db1234ff99b5
Ilia <35358958+My-Duck@users.noreply.github.com>
2024-03-29 15:02:02 +0500

    Create runme.sh

---
+++ runme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+python3 file_maker.py;
+gcc main.c -o main;
+./main A B;
+gzip -c A > A.gz;
+gzip -c B > B.gz;
+gunzip -cd B.gz | ./main C;
+./main A D -b 100;
+stat A > result.txt;
+stat A.gz >> result.txt;
+stat B >> result.txt;
+stat B.gz >> result.txt;
+stat C >> result.txt;
+stat D >> result.txt;
+cat result.txt;
